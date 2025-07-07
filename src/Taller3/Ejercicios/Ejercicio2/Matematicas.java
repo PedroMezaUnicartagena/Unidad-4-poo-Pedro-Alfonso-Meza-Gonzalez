932c3a6967c5073e2cc743a5de8615ac9bf8b0bc
PedroMezaUnicartagena <pmezag@unicartagena.edu.co>
2025-07-07 18:35:19 -0500

    feat: Pedro Meza - ejercicio 2 clase matematicas con metodos static taller 3

---
+++ src/Taller3/Ejercicios/Ejercicio2/Matematicas.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Taller3.Ejercicios.Ejercicio2;
+
+/**
+ *
+ * @author HP
+ */
+public class Matematicas {
+
+    public static double suma(double a, double b) {
+        return a + b;
+    }
+
+    public static double resta(double a, double b) {
+        return a - b;
+    }
+
+    public static double multiplicacion(double a, double b) {
+        return a * b;
+    }
+
+    public static double division(double a, double b) {
+        return a / b;
+    }
+}
