25b7dbbc21d29176633281296cdd0b927985cef9
PedroMezaUnicartagena <pmezag@unicartagena.edu.co>
2025-07-08 13:17:13 -0500

    feat Pedro Meza - ejercicio 3 clase utilidades con metodos publicos taller 7

---
+++ src/Taller7/Ejercicios/Ejercicio3/Utilidades.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Taller7.Ejercicios.Ejercicio3;
+
+/**
+ *
+ * @author HP
+ */
+public class Utilidades {
+    public static double suma(double a, double b) {
+        return a + b;
+    }
+
+    public static double resta(double a, double b) {
+        return a - b;
+    }
+
+    public static double multiplicacion(double a, double b) {
+        return a * b;
+    }
+
+    public static double division(double a, double b) {
+        return a / b;
+    }
+}
