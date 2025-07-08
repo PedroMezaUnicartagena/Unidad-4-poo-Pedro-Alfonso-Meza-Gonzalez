6e4099bc5feda193afb249f5393881ef2e704e54
PedroMezaUnicartagena <pmezag@unicartagena.edu.co>
2025-07-08 15:04:02 -0500

    feat: Pedro Meza - ejercicio 4 uso incorrecto de herencia con errores de compilacion taller 8

---
+++ src/Taller8/Ejercicios/Ejercicio4/Acuatico.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Taller8.Ejercicios.Ejercicio4;
+
+/**
+ *
+ * @author HP
+ */
+public class Acuatico {
+    protected void nadar(){
+        System.out.println("Nadando");
+    }
+}
