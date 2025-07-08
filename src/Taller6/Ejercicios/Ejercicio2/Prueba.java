51644c40bfa3eafbf4f7537129f6cb608e166b0d
PedroMezaUnicartagena <pmezag@unicartagena.edu.co>
2025-07-08 12:06:09 -0500

    feat: Pedro Meza - ejercicio 2 intentar acceder a atributos protected directamente taller 6

---
+++ src/Taller6/Ejercicios/Ejercicio2/Prueba.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Taller6.Ejercicios.Ejercicio2;
+
+/**
+ *
+ * @author HP
+ */
+public class Prueba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Moto moto = new Moto(250, "deportiva", "honda");
+        
+        // al intentar acceder directamente a los atributos protected da error
+        // moto.marca = "akt";.
+    }
+    
+}
