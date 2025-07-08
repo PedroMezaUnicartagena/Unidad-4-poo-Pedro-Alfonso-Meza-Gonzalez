6d6a76957331decd1ea2debdf781bfd20df09896
PedroMezaUnicartagena <pmezag@unicartagena.edu.co>
2025-07-08 10:30:51 -0500

    feat: Pedro Meza - Ejercicio 2 crear una clase vehiculo y moto con modificador de paquete e intentar acceder desde otro paquete taller 5

---
+++ src/Taller5/Ejercicios/Ejercicico2/Vehiculos/Moto.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Taller5.Ejercicios.Ejercicico2.Vehiculos;
+
+/**
+ *
+ * @author HP
+ */
+public class Moto extends Vehiculo{
+    int cilindrada;
+
+    public Moto(int cilindrada, String tipo) {
+        super(tipo);
+        this.cilindrada = cilindrada;
+    }
+    
+    
+}
