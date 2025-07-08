ad9b44ba2d8aa75fcde5b32b9852cf1235a10165
PedroMezaUnicartagena <pmezag@unicartagena.edu.co>
2025-07-08 14:40:07 -0500

    feat: Pedro Meza - ejercicio 2 clase estudiante persona con sobreescritura de metodos taller 8

---
+++ src/Taller8/Ejercicios/Ejercicio2/Main.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Taller8.Ejercicios.Ejercicio2;
+
+/**
+ *
+ * @author HP
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Estudiante estudiante = new Estudiante("1824-CK", "Dario", 25);
+        estudiante.mostrarInformacion();
+    }
+    
+}
---
+++ src/Taller8/Ejercicios/Ejercicio2/Persona.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Taller8.Ejercicios.Ejercicio2;
+
+/**
+ *
+ * @author HP
+ */
+public class Persona {
+
+    protected String nombre;
+    protected int edad;
+
+    public Persona(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        if (edad > 0) {
+            this.edad = edad;
+        }
+    }
+
+    protected void mostrarInformacion(){
+        System.out.println("Nombre: "+nombre);
+        System.out.println("Edad: "+edad);
+    }
+}
