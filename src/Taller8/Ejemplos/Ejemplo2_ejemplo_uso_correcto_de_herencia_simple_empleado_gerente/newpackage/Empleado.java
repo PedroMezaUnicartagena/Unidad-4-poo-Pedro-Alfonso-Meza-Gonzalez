682c506b28cf3b6c1a76165be46524f8b25441ea
PedroMezaUnicartagena <pmezag@unicartagena.edu.co>
2025-07-08 13:54:53 -0500

    feat: Pedro Meza - ejemplo2 uso correcto de herencia empleado gerente taller 8

---
+++ src/Taller8/Ejemplos/Ejemplo2_ejemplo_uso_correcto_de_herencia_simple_empleado_gerente/newpackage/Empleado.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Taller8.Ejemplos.Ejemplo2_ejemplo_uso_correcto_de_herencia_simple_empleado_gerente.newpackage;
+
+/**
+ *
+ * @author HP
+ */
+public class Empleado {
+
+    protected String nombre;
+    protected double salario;
+
+    public Empleado(String nombre, double salario) {
+        this.nombre = nombre;
+        this.salario = salario;
+    }
+
+    public void mostrarDetalles() {
+        System.out.println("Nombre: " + nombre + ", Salario: " + salario);
+    }
+}
---
+++ src/Taller8/Ejemplos/Ejemplo2_ejemplo_uso_correcto_de_herencia_simple_empleado_gerente/newpackage/Gerente.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Taller8.Ejemplos.Ejemplo2_ejemplo_uso_correcto_de_herencia_simple_empleado_gerente.newpackage;
+
+/**
+ *
+ * @author HP
+ */
+public class Gerente extends Empleado {
+
+    private String departamento;
+
+    public Gerente(String nombre, double salario, String departamento) {
+        super(nombre, salario);
+        this.departamento = departamento;
+    }
+
+    @Override
+    public void mostrarDetalles() {
+        super.mostrarDetalles();
+        System.out.println("Departamento: " + departamento);
+    }
+}
