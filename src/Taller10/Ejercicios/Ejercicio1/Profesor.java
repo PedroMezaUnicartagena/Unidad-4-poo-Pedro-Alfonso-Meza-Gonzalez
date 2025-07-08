d52ec7eaab147a35e09861ec4dd5be8401456fdc
PedroMezaUnicartagena <pmezag@unicartagena.edu.co>
2025-07-08 16:32:21 -0500

    feat: Pedro Meza - ejercicio 1 sobreescribir el metodpo presentarse y usar polimorfismo taller 10

---
+++ src/Taller10/Ejercicios/Ejercicio1/Profesor.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Taller10.Ejercicios.Ejercicio1;
+
+/**
+ *
+ * @author HP
+ */
+public class Profesor extends Persona {
+
+    @Override
+    public void presentarse() {
+        System.out.println("Hola soy el profesor");
+    }
+
+}
