b0ce0cf1f01feb10b65d3c09a5134b012ab694a2
PedroMezaUnicartagena <pmezag@unicartagena.edu.co>
2025-07-08 18:26:48 -0500

    feat: Pedro Meza - ejemplo 3 uso de metodos predeterminados en interfaces taller 12

---
+++ src/Taller12/Ejemplos/Ejemplio3_uso_de_metodos_predeterminados_en_interfaces/Coche.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Taller12.Ejemplos.Ejemplio3_uso_de_metodos_predeterminados_en_interfaces;
+
+/**
+ *
+ * @author HP
+ */
+public class Coche implements Vehiculo {
+
+    @Override
+    public void acelerar() {
+        System.out.println("El coche está acelerando.");
+    }
+}
