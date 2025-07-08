9c0e866437d87c723648297d6a867195a5b74729
PedroMezaUnicartagena <pmezag@unicartagena.edu.co>
2025-07-08 18:32:50 -0500

    feat: Pedro Meza - ejemplo 4 uso incorrecto de interfaz (no implementar todos los metodos) taller 12

---
+++ src/Taller12/Ejemplos/Ejemplio4_uso_incorrecto_de_interfaces_no_implementar_todos_los_metodos/Avion.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Taller12.Ejemplos.Ejemplio4_uso_incorrecto_de_interfaces_no_implementar_todos_los_metodos;
+
+/**
+ *
+ * @author HP
+ */
+public class Avion implements Volador {
+// Error de compilación: porque no implementa el método volar() de la interfaz
+}
---
+++ src/Taller12/Ejemplos/Ejemplio4_uso_incorrecto_de_interfaces_no_implementar_todos_los_metodos/Volador.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Taller12.Ejemplos.Ejemplio4_uso_incorrecto_de_interfaces_no_implementar_todos_los_metodos;
+
+/**
+ *
+ * @author HP
+ */
+public interface Volador {
+
+    void volar();
+}
