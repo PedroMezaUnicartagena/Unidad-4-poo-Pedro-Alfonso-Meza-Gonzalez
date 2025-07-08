b3524b1acbba90c97edfbbbe7e963a4ee1f5717b
PedroMezaUnicartagena <pmezag@unicartagena.edu.co>
2025-07-08 17:10:11 -0500

    feat: Pedro Meza - ejemplo 3 uso erroneo de clases abstractas taller 11

---
+++ src/Taller11/Ejemplos/Ejemplo3_uso_incorrecto_sin_errores_de_compilacion/Gato.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Taller11.Ejemplos.Ejemplo3_uso_incorrecto_sin_errores_de_compilacion;
+
+/**
+ *
+ * @author HP
+ */
+/*
+aunque en el ejemplo del profesor dice que no produce un error de compilacion
+en realidad si al no sobreescribir el metodo abstracto
+*/
+public class Gato extends Animal{
+    
+}
