0849b86d3b6e19492cccae0acf779014e1aa1197
PedroMezaUnicartagena <pmezag@unicartagena.edu.co>
2025-07-08 14:26:48 -0500

    feat: Pedro Meza - ejemplo 5 uso incorrecto de la herencia (no llamar al constructor de la clase padre con super) taller 8

--- src/Taller8/Ejemplos/Ejemplo2_ejemplo_uso_correcto_de_herencia_simple_empleado_gerente/Empleado.java
+++ src/Taller8/Ejemplos/Ejemplo2_ejemplo_uso_correcto_de_herencia_simple_empleado_gerente/Empleado.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Taller8.Ejemplos.Ejemplo2_ejemplo_uso_correcto_de_herencia_simple_empleado_gerente.newpackage;
+package Taller8.Ejemplos.Ejemplo2_ejemplo_uso_correcto_de_herencia_simple_empleado_gerente;
 
 /**
  *
---
+++ src/Taller8/Ejemplos/Ejemplo5_uso_incorrecto_no_usar_super_para_llamar_al_constructor_de_la_clase_padre/Animal.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Taller8.Ejemplos.Ejemplo5_uso_incorrecto_no_usar_super_para_llamar_al_constructor_de_la_clase_padre;
+
+/**
+ *
+ * @author HP
+ */
+public class Animal {
+
+    private String especie;
+
+    public Animal(String especie) {
+        this.especie = especie;
+    }
+}
