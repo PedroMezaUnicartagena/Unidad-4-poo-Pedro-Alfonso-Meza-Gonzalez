5ed20a4ad4e2129c33073144ad9d6f60487f225e
PedroMezaUnicartagena <pmezag@unicartagena.edu.co>
2025-07-08 15:17:41 -0500

    feat: Pedro Meza - ejemplo 1 uso correcto de super taller 9

---
+++ src/Taller9/Ejemplos/Ejemplo1_uso_correcto_de_super/Animal.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Taller9.Ejemplos.Ejemplo1_uso_correcto_de_super;
+
+/**
+ *
+ * @author HP
+ */
+public class Animal {
+
+    protected String especie;
+
+    public Animal(String especie) {
+        this.especie = especie;
+    }
+
+    public void mostrarEspecie() {
+        System.out.println("Especie: " + especie);
+    }
+}
