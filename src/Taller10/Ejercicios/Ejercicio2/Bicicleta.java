d71e0be31127e5772bf969c17ad05a706c5e8655
PedroMezaUnicartagena <pmezag@unicartagena.edu.co>
2025-07-08 16:37:38 -0500

    feat: Pedro Meza - Ejercicio 2 crear clases vehiculos y bicicleta y sobreescribir el metodo moverse taller 10

---
+++ src/Taller10/Ejercicios/Ejercicio2/Bicicleta.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Taller10.Ejercicios.Ejercicio2;
+
+/**
+ *
+ * @author HP
+ */
+public class Bicicleta extends Vehiculo {
+
+    @Override
+    public void moverse() {
+        System.out.println("La Bicicleta se esta moviendo");
+    }
+
+}
