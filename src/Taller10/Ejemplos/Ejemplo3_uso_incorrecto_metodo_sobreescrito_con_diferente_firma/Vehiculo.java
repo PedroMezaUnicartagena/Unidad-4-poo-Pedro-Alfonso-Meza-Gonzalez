bbbf435f38aa833e23cae8a35c32b7cdd7f3e0e1
PedroMezaUnicartagena <pmezag@unicartagena.edu.co>
2025-07-08 16:09:09 -0500

    feat: Pedro Meza - ejemplo 3 uso incorrecto de la sobreescritura de metodos (metodo con diferente firma) taller 10

---
+++ src/Taller10/Ejemplos/Ejemplo3_uso_incorrecto_metodo_sobreescrito_con_diferente_firma/Vehiculo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Taller10.Ejemplos.Ejemplo3_uso_incorrecto_metodo_sobreescrito_con_diferente_firma;
+
+/**
+ *
+ * @author HP
+ */
+public class Vehiculo {
+
+    public void acelerar(int velocidad) {
+        System.out.println("El vehículo acelera a " + velocidad + " km/h.");
+    }
+}
