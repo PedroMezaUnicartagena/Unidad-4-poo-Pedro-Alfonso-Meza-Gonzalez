f472472e0659d8402952276c0af7c98193ac9ae7
PedroMezaUnicartagena <pmezag@unicartagena.edu.co>
2025-07-08 14:08:38 -0500

    feat: Pedro Meza -  ejemplo 4 uso incorrecto de herencia (intentar a acceder a taributos privados de la clase padre) taller 8

---
+++ src/Taller8/Ejemplos/Ejemplo4_uso_incorrecto_intentar_acceder_a_atributos_privados_de_la_clase_padre/Vehiculo.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Taller8.Ejemplos.Ejemplo4_uso_incorrecto_intentar_acceder_a_atributos_privados_de_la_clase_padre;
+
+/**
+ *
+ * @author HP
+ */
+public class Vehiculo {
+
+    private String marca;
+
+    public Vehiculo(String marca) {
+        this.marca = marca;
+    }
+}
