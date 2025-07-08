faf92e72a3e04924c03f5f0a037bbc14afdea858
PedroMezaUnicartagena <pmezag@unicartagena.edu.co>
2025-07-08 15:47:21 -0500

    feat: Pedro Meza - ejercicio 3 uso incorrecto de super taller 9

---
+++ src/Taller9/Ejercicios/Ejercicio3/Electrodomestico.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Taller9.Ejercicios.Ejercicio3;
+
+/**
+ *
+ * @author HP
+ */
+public class Electrodomestico {
+    private String tipoCorriente;
+
+    public Electrodomestico(String tipoCorriente) {
+        this.tipoCorriente = tipoCorriente;
+    }
+
+    public String getTipoCorriente() {
+        return tipoCorriente;
+    }
+
+    public void setTipoCorriente(String tipoCorriente) {
+        this.tipoCorriente = tipoCorriente;
+    }
+    
+    
+}
---
+++ src/Taller9/Ejercicios/Ejercicio3/Main.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Taller9.Ejercicios.Ejercicio3;
+
+/**
+ *
+ * @author HP
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        // uso incorrecto de super no se puede usar fuera del contexto de la clase derivada
+        super.getTipoCorriente();
+    }
+    
+}
