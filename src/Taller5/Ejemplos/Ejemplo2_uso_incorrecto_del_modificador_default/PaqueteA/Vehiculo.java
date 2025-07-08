c67d0321d375f7de3c09d16df5f14bc84dfe7f4e
PedroMezaUnicartagena <pmezag@unicartagena.edu.co>
2025-07-08 09:58:41 -0500

    feat: Pedro Meza - ejemplo 2 uso incorrecto del modificador default con errores de compilacion (intentar acceder desde otro paquete) taller 5

---
+++ src/Taller5/Ejemplos/Ejemplo2_uso_incorrecto_del_modificador_default/PaqueteA/Vehiculo.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Taller5.Ejemplos.Ejemplo2_uso_incorrecto_del_modificador_default.PaqueteA;
+
+/**
+ *
+ * @author HP
+ */
+class Vehiculo {
+
+    String marca;
+
+    Vehiculo(String marca) {
+        this.marca = marca;
+    }
+}
---
+++ src/Taller5/Ejemplos/Ejemplo2_uso_incorrecto_del_modificador_default/PaqueteB/Main.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Taller5.Ejemplos.Ejemplo2_uso_incorrecto_del_modificador_default.PaqueteB;
+
+/**
+ *
+ * @author HP
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // esto causara un error debido a que vehiculo no es visible desde otros paquetes
+        //Vehiculo v = new Vehiculo("Toyota");
+    }
+
+}
