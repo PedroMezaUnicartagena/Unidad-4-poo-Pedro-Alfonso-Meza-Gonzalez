8254269defe89580e47a5b38423d31b83dcf676b
PedroMezaUnicartagena <pmezag@unicartagena.edu.co>
2025-07-08 20:07:08 -0500

    feat: Pedro Meza - Ejercicio 2 uso de interfaces taller 13

---
+++ src/Taller13/Ejercicios/Ejercicio2/Prueba.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Taller13.Ejercicios.Ejercicio2;
+
+/**
+ *
+ * @author HP
+ */
+public class Prueba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Pato pato = new Pato();
+        pato.nadar();
+        pato.volar();
+    }
+    
+}
