2d4ae4a6e1fced3eacc3da5b1fd63fca2458dea3
PedroMezaUnicartagena <pmezag@unicartagena.edu.co>
2025-07-08 20:39:05 -0500

    feat: Pedro Meza - ejemplo  1 uso correcto de polimorfismo taller 14

---
+++ src/Taller14/Ejemplos/Ejemplo1_uso_correcto_de_polimorfismo/Ingeniero.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Taller14.Ejemplos.Ejemplo1_uso_correcto_de_polimorfismo;
+
+/**
+ *
+ * @author HP
+ */
+public class Ingeniero extends Empleado {
+
+    @Override
+    public void trabajar() {
+        System.out.println("El ingeniero está diseñando.");
+    }
+}
