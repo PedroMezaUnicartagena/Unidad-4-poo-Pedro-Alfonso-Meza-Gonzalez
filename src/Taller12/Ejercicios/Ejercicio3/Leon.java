9cea61e33b5c6a25234021640749525d3b967bf3
PedroMezaUnicartagena <pmezag@unicartagena.edu.co>
2025-07-08 19:08:16 -0500

    feat: Pedro Meza - ejercicio 3 uso incorrecto de interfaces taller 12

---
+++ src/Taller12/Ejercicios/Ejercicio3/Leon.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Taller12.Ejercicios.Ejercicio3;
+
+/**
+ *
+ * @author HP
+ */
+public class Leon implements Oviparo {
+
+    @Override
+    public void ponerHuevos() {
+        // la clase leon no debe implementar la interfaz oviparo
+        System.out.println("Los leones no ponen huevos");
+    }
+    
+}
