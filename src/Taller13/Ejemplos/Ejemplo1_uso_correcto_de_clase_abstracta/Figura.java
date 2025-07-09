306c6a1c45849c28ff8ac36137c271764d9a4596
PedroMezaUnicartagena <pmezag@unicartagena.edu.co>
2025-07-08 19:31:58 -0500

    feat: Pedro Meza - ejemplo 1 uso correcto de clase abstracta taller 13

---
+++ src/Taller13/Ejemplos/Ejemplo1_uso_correcto_de_clase_abstracta/Figura.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Taller13.Ejemplos.Ejemplo1_uso_correcto_de_clase_abstracta;
+
+/**
+ *
+ * @author HP
+ */
+public abstract class Figura {
+
+    // Método abstracto
+    public abstract double calcularArea();
+
+    public void mostrarFigura() {
+        System.out.println("Mostrando figura.");
+    }
+}
