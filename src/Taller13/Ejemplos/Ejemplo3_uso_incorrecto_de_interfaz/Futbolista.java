c36cf700c15db959f7fb8441e75630fc9fe8da0d
PedroMezaUnicartagena <pmezag@unicartagena.edu.co>
2025-07-08 19:38:33 -0500

    feat: Pedro Meza - ejemplo 3 uso incorrecto de interfaz taller 13

---
+++ src/Taller13/Ejemplos/Ejemplo3_uso_incorrecto_de_interfaz/Futbolista.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Taller13.Ejemplos.Ejemplo3_uso_incorrecto_de_interfaz;
+
+/**
+ *
+ * @author HP
+ */
+public class Futbolista implements Jugador {
+// Error de compilación:porque La clase futbolistano implementa el método jugar()
+}
