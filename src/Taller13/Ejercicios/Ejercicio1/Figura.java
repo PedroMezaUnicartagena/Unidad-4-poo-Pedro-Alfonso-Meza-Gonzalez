65fbef48206a9223fc225852bd8cfe0102ca2b95
PedroMezaUnicartagena <pmezag@unicartagena.edu.co>
2025-07-08 19:58:43 -0500

    feat: Pedro Meza - ejercicio 1 clase abstracta con implementaciones diferentes en clases derivadas taller 13

---
+++ src/Taller13/Ejercicios/Ejercicio1/Figura.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Taller13.Ejercicios.Ejercicio1;
+
+/**
+ *
+ * @author HP
+ */
+public abstract class Figura {
+   public abstract double calcularArea();
+}
