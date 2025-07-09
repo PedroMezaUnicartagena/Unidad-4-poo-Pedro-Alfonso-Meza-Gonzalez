e8257cff113511241a6a716caf833f89a4cf9bfd
PedroMezaUnicartagena <pmezag@unicartagena.edu.co>
2025-07-08 21:38:28 -0500

    feat: Pedro Meza - ejercicio 4 polimorfismo con clases vehiculo y derivadas taller 14

---
+++ src/Taller14/Ejercicios/Ejercicio4/Prueba.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Taller14.Ejercicios.Ejercicio4;
+
+/**
+ *
+ * @author HP
+ */
+public class Prueba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Vehiculo coche = new Coche();
+        Vehiculo bicicleta = new Bicicleta();
+        
+        coche.mover();
+        bicicleta.mover();
+    }
+    
+}
