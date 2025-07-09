79ba48fe650d829227683af525f096ebe8de448c
PedroMezaUnicartagena <pmezag@unicartagena.edu.co>
2025-07-08 21:22:39 -0500

    feat: Pedro Meza - ejercicio 3 uso erroneo de polimorfismo taller 14

---
+++ src/Taller14/Ejercicios/Ejercicio3/Electrodomestico.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Taller14.Ejercicios.Ejercicio3;
+
+/**
+ *
+ * @author HP
+ */
+public class Electrodomestico {
+    public void funcionar(){
+        System.out.println("El electrodomestico funciona");
+    }
+}
