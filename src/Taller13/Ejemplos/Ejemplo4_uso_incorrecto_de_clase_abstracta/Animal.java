0252a76294fe1d5b1f1c949a62400207d05613c8
PedroMezaUnicartagena <pmezag@unicartagena.edu.co>
2025-07-08 19:42:16 -0500

    feat: Pedro Meza - ejemplo 4 uso incorrecto de clases abstractas taller 13

---
+++ src/Taller13/Ejemplos/Ejemplo4_uso_incorrecto_de_clase_abstracta/Animal.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Taller13.Ejemplos.Ejemplo4_uso_incorrecto_de_clase_abstracta;
+
+/**
+ *
+ * @author HP
+ */
+public abstract class Animal {
+
+    public abstract void hacerSonido();
+}
